a494f288bddc40553d90300e817e37e26111fbe8
DevinAlexander <devinmalexander@gmail.com>
2020-11-07 06:48:43 -0600

    [CHORE] Created band.js and song.js models

---
+++ app/models/band.js
@@ -0,0 +1,14 @@
+// app/models/band.js
+import { tracked } from '@glimmer/tracking';
+
+export default class Band {
+@tracked name;
+@tracked songs;
+
+    constructor({ id, name, songs }, relationships={}) {
+        this.id = id;
+        this.name = name;
+        this.songs = songs ?? [];
+            this.relationships = relationships;
+    }
+}
---
+++ app/models/song.js
@@ -0,0 +1,14 @@
+import { tracked } from '@glimmer/tracking';
+
+export default class Song {
+    title = '';
+    @tracked rating = 0;
+    band = null;
+
+    constructor({ title, rating, band }, relationships={}) {
+        this.title = title;
+        this.rating = rating;
+        this.band = band;
+        this.relationships = relationships;
+    }
+}
